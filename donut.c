caa0f2ed418acafc58865ccd9edcec51336d1a84
alexanderono7 <alexanderono7@gmail.com>
2024-06-04 12:20:26 -0700

    2D points of cross section can be calculated

--- donut.c
+++ donut.c
@@ -6,17 +6,20 @@
 const int output_width = 300;
 const int output_height = 200;
 
-const float theta_inc = 0.07;
 const float phi_inc = 0.02;
+const float theta_inc = 0.07;
 
-const float r1 = 5; // size of donut hole
-const float r2 = 2; // thickness of donut cross sections
+const float r1 = 2; // thickness of donut cross sections
+const float r2 = 5; // size of donut hole
+
 
 void render(){
     char arr[output_width][output_height];
     for (float phi = 0; phi < (2*pi); phi+=phi_inc){
         for (float theta = 0; theta < (2*pi); theta+=theta_inc){
-
+            float x = r2 + r1*cos(theta);
+            float y = r1*sin(theta);
+            printf("(%.2f, %.2f)\n",x,y);
         }
     }
 }
