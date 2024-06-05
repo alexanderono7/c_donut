26446265bd140163a305e999ecc6dece0b378047
alexanderono7 <alexanderono7@gmail.com>
2024-06-04 20:29:35 -0700

    3D rotation animations possibly working

--- donut.c
+++ donut.c
@@ -35,23 +35,24 @@ void render(float A, float B){
             // matrix multiplying by 3D rotation matrix, rotational extrusion (y remains unchanged because donut extrudes around y-axis)
             x = x*cos(phi);
             float z = (r2 + r1*cos(theta))*sin(phi); // is this supposed to be negative??
-            z = z + k2; // make the object further away, so eye at the origin can actually see the whole thing
 
             // matrix multiplying- applying rotation matrices for animated rotation of entire donut
-            x = x * (cos(B)*cos(phi) + sin(A)*sin(B)*sin(phi)) - r1*cos(A)*sin(B)*sin(theta);
-            y = (r2+y)*(cos(phi)*sin(B)-cos(B)*sin(A)*sin(phi)) + r1*cos(A)*cos(B)*sin(theta);
-            z = cos(A)*z + r1*sin(A)*sin(theta);
+            //x = x * (cos(B)*cos(phi) + sin(A)*sin(B)*sin(phi)) - r1*cos(A)*sin(B)*sin(theta);
+            x = (r2+r1*cos(theta)) * (cos(B)*cos(phi) + sin(A)*sin(B)*sin(phi)) - r1*cos(A)*sin(B)*sin(theta);
+            y = (r2+r1*cos(theta))*(cos(phi)*sin(B)-cos(B)*sin(A)*sin(phi)) + r1*cos(A)*cos(B)*sin(theta);
+            z = cos(A)*(r2+r1*cos(theta))*sin(phi) + r1*sin(A)*sin(theta);
+
+            z = z + k2; // make the object further away, so eye at the origin can actually see the whole thing
 
             // projection of the 3D object to our 2D screen
             int xp = rintf(k1*x/(z));
             xp+= output_width/2;
             int yp = rintf(k1*y/(z));
             yp+= output_height/2;
-            //yp += output_height/2;
             arr[yp][xp] = '.';
 
             // for debugging; delete later:
-            /*
+            //*
             for(int i = (output_height-1); i > 0; --i){
                 for(int j = 0; j < output_width; ++j){
                     printf("%c",arr[i][j]);
@@ -59,7 +60,7 @@ void render(float A, float B){
                 printf("\n");
             }
             //usleep(200);
-            */
+            //*/
 
         }
     }
@@ -70,11 +71,16 @@ void render(float A, float B){
         }
         printf("\n");
     }
+    return;
 }
 
 int main() {
-    for (float a = 0; a < 2*pi ; a+=0.01){
-        render(a,a);
-    }
+    render(0,0);
+    //for (float a = 0; a < 16*pi ; a+=0.03){
+        //for (float b = 0; b < 16*pi ; b+=0.03){
+        //printf("a: %f",a);
+        //printf("b: %f",b);
+        //}
+    //}
     return 0;
 }
